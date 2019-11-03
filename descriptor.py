ffa8ef20c83602fcef5630b77f411292714e27e0
lehabatareya <liohich@yandex.ru>
2019-11-04 00:18:09 +0300

    Added dummy class descriptions to descriptor.py file

--- descriptor.py
+++ descriptor.py
@@ -1,1 +1,37 @@
-class Quantity():
+"""
+Descriptors are classes to abstract the class instance attributes behavior.
+This is the example descriptor from perfect presentation:
+'Python: Encapsulation with Descriptors'
+by Luciano Ramalho
+"""
+
+
+class Quantity:
+    """
+    Descriptor class for nonzero quantities
+    """
+    def __init__(self):
+        self.field_name = ''
+
+    def __set__(self, instance, value):
+        pass
+
+    def __get__(self, instance, owner):
+        pass
+
+
+class ShopItem:
+    """
+    Simple class of shop item with given weight and given
+    """
+    def __init__(self, weight, value):
+        self.quantity = Quantity()
+        self.value = Quantity()
+
+    def get_cost(self):
+        """Get the total cost of the shop item"""
+        return self.quantity * self.value
+
+
+if __name__ == '__main__':
+    carrot = ShopItem(10, 13)
